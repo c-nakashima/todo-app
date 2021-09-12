9e0dcf43336ca5684fa37cad1759b8b61a06d330
c-nakashima <chisatonakashima1025@gmail.com>
2021-09-12 16:41:59 +0900

    revise vue.config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  outputDir: 'docs/',
-  assetsDir: './',
-  publicPath: './'
+  publicPath: '/github-pages-example-with-vue-router',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false,
 }
